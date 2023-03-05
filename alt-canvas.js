daa863e36c221528e4497974b03d13bbee9fdd28
ArdaGurcan <ardagurcan2004@gmail.com>
2023-03-05 08:12:30 -0600

    changing imgs

--- alt-canvas.js
+++ alt-canvas.js
@@ -608,61 +608,71 @@ var h = grid.length;
 var d = 10;
 var result;
 function setup() {
-  var graph = new Graph(
-		grid
-	); 
-	var start = graph.grid[37][2];
-	var end = graph.grid[8][11];
-	result = astar.search(graph, start, end);
-  console.log(result)
+  var graph = new Graph(grid);
+  var start = graph.grid[12][1];
+  var end = graph.grid[8][11];
+  result = astar.search(graph, start, end);
+  console.log(result);
 
-	
   var myCanvas = createCanvas(500, 500);
-    myCanvas.parent("newCanvas");
-    background(50);
-    // rectMode(CENTER);
-    stroke(0);
-    result.forEach(node => {
-      grid[node.x][node.y] = 2
-    });
-    for (let y = 0; y < h; y++) {
-      for (let x = 0; x < w; x++) {
-        if (grid[y][x] == 1)
-          fill(50)
-        else if (grid[y][x] == 0)
-          fill(255)
-        else if (grid[y][x] == 2)
-          fill('orange')
-        rect(x * d, y * d, d, d);
-      }
+  myCanvas.parent("newCanvas");
+  background(50);
+  // rectMode(CENTER);
+  stroke(0);
+  result.forEach(node => {
+    grid[node.x][node.y] = 2;
+  });
+  for (let y = 0; y < h; y++) {
+    for (let x = 0; x < w; x++) {
+      if (grid[y][x] == 1) fill(50);
+      else if (grid[y][x] == 0) fill(255);
+      else if (grid[y][x] == 2) fill("orange");
+      rect(x * d, y * d, d, d);
     }
+  }
 }
 
 ///////////// (j * 10 + i) = row number plus x position
-function draw() {
-}
+function draw() {}
 
-function mouseClicked() {
-  let hotspots = [[2,12, "IMG_2785.jpg"], [2,24, "IMG_2784.jpg"], [2,32, "IMG_2783.jpg"], [13,28, "IMG_2781.jpg"], [13,33, "IMG_2782.jpg"], [13, 24, "IMG_2782.jpg"]]
+function mousePressed() {
+  if (mouseX > 0 && mouseX < 500 && mouseY > 0 && mouseY < 500) {
+    let hotspots = [
+      [2, 12, "IMG_2785.jpg"],
+      [2, 24, "IMG_2784.jpg"],
+      [2, 32, "IMG_2783.jpg"],
+      [13, 28, "IMG_2781.jpg"],
+      [13, 33, "IMG_2782.jpg"],
+      [13, 24, "IMG_2782.jpg"],
+    ];
 
-  let smallestDistance = 100000;
-  let closest = [2,12]
+    let smallestDistance = 100000;
+    let closest = [2, 12];
 
-  for (let i = 0; i < hotspots.length; i++) {
-    let dist = (mouseY - hotspots[i][1]*d)**2 + (mouseX - hotspots[i][0]*d)**2
-    if (dist < smallestDistance) {   
-      smallestDistance = dist
-      closest = hotspots[i];
+    for (let i = 0; i < hotspots.length; i++) {
+      let dist = (mouseY - hotspots[i][1] * d) ** 2 + (mouseX - hotspots[i][0] * d) ** 2;
+      if (dist < smallestDistance) {
+        smallestDistance = dist;
+        closest = hotspots[i];
+      }
     }
-  }  
 
-  fill('orange')
-
-  ellipse(closest[0]*d,closest[1]*d, 8, 8)
-  // Math.floor(mouseX/d)*d + d/2, Math.floor(mouseY/d)*d + d/2
-  console.log( Math.floor(mouseX/d) + ", " +  Math.floor(mouseY/d))
+    // if (smallestDistance < 9 * d * d) {
+    canvasHandler(closest[2]);
+    // }
+    for (let y = 0; y < h; y++) {
+      for (let x = 0; x < w; x++) {
+        if (grid[y][x] == 1) fill(50);
+        else if (grid[y][x] == 0) fill(255);
+        else if (grid[y][x] == 2) fill("orange");
+        rect(x * d, y * d, d, d);
+      }
+    }
+    fill("red")
+    ellipse(closest[0] * d, closest[1] * d, 8, 8);
+    // Math.floor(mouseX/d)*d + d/2, Math.floor(mouseY/d)*d + d/2
+    console.log(Math.floor(mouseX / d) + ", " + Math.floor(mouseY / d));
+  }
   // prevent default
   return false;
 }
-
-
--- panorama.js
+++ panorama.js
@@ -1,22 +1,15 @@
-let images = new Array();
-images.push("https://pannellum.org/images/cerro-toco-0.jpg")
-images.push("https://pannellum.org/images/alma.jpg")
-images.push("https://pannellum.org/images/jfk.jpg")
-images.push("https://i.imgur.com/G7t9QD9.jpg")
 
 // let viewer = Renderer('panorama')
 this.viewer = pannellum.viewer('panorama', {
     type: 'equirectangular',
-    panorama: images[0],
+    panorama: "IMG_2785.jpg",
     autoLoad: true,
 })
 
-function canvasHandler(index) {
-    console.log(index);
-
+function canvasHandler(fname) {
     this.viewer = pannellum.viewer('panorama', {
     type: 'equirectangular',
-    panorama: images[index % images.length],
+    panorama: fname,
     autoLoad: true,
     })
 }
